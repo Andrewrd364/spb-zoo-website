75cbed0591bd1d1148681d610bd1c7b176c7b52f
andre <andrey.liskov03@mail.ru>
2024-10-01 01:40:21 +0500

    deploy to render

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -1,13 +1,25 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DB_NAME, 
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD, 
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true, 
+                rejectUnauthorized: false
+            }
+        },
+        pool: {
+            max: 5, 
+            min: 0, 
+            acquire: 30000,
+            idle: 10000
+        }
     }
 );
 
