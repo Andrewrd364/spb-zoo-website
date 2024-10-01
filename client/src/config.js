a608f24cee698f0d7d014ef23957f9fb361d5ed8
andre <andrey.liskov03@mail.ru>
2024-10-01 12:37:23 +0500

    Обновил config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // src/constants.js
-export const IMAGE_URL = "https://spb-zoo-website-production.up.railway.app//";
+export const IMAGE_URL = "https://spb-zoo-website-production.up.railway.app/";
 export const API_URL = IMAGE_URL + 'api';
